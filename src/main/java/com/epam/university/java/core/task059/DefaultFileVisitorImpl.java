e6cbac92783c4c08ee4a8b29392a8e4c5e3b0614
Svetlana Perekrestova <Sherlock.Nacht@gmail.com>
2020-11-15 19:03:00 +0300

    task059 is resolved

---
+++ src/main/java/com/epam/university/java/core/task059/DefaultFileVisitorImpl.java
@@ -0,0 +1,34 @@
+package com.epam.university.java.core.task059;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DefaultFileVisitorImpl extends DefaultFileVisitor {
+    private final List<String> foundFiles = new ArrayList<>();
+
+    private String partOfContent;
+    private boolean partOfContentCheck;
+
+    @Override
+    public List<String> getResult() {
+        return foundFiles;
+    }
+
+    public void setPartOfContent(String partOfContent) {
+        this.partOfContent = partOfContent;
+        this.partOfContentCheck = true;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (partOfContentCheck && new String(Files.readAllBytes(file)).contains(partOfContent)) {
+            foundFiles.add(String.valueOf(file));
+        }
+        return FileVisitResult.CONTINUE;
+    }
+}
